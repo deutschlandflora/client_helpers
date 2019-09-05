26c2749e854414a7dcad32ae69da91d8d90416cd
jbrueck <maps4net@web.de>
2019-09-05 20:02:18 +0200

    Add files via upload

---
+++ prebuilt_forms/lang/distribution_map_1.de.php
@@ -0,0 +1,67 @@
+﻿<?php
+/**
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ *
+ * @package	Client
+ * @author	Indicia Team
+ * @license	http://www.gnu.org/licenses/gpl.html GPL 3.0
+ * @link 	http://code.google.com/p/indicia/
+ */
+
+global $custom_terms;
+/**
+ * Additional language terms or overrides for dynamic_sample_occurrence form.
+ *
+ * @package	Client
+ */
+$custom_terms =  array(
+		
+	'New report' => 'Neuer Bericht',
+	'Species' => 'Art',
+	'Source' => 'Quelle',
+	'Comment' => 'Kommentar',
+	'Filter' => 'Filter',
+	'My records' => 'Meine Daten',
+	'Common Name' => 'Deutscher Name',
+	'Images'=>'Bilder',
+  'Taxon Group' => 'Artgruppe',
+  'Recorder' => 'Kartierer',
+  'Date' => 'Datum',
+	'Select filter' => 'Filter auswählen',
+	'Create a filter' => 'Filter erstellen',
+	'Site name' => 'Naturraum', 
+	'Showing records' => 'Zeige Datensätze',
+	'Download this report' => 'Diesen Bericht downloaden',
+	'Apply'=>'Anwenden',
+	'Modify filter' => 'Filter anpassen',
+  'Reset'=>'Zurücksetzen',
+  'Grid Ref'=>'Raum',
+  'Preferred Name'=>'Wissensch. Name',
+  'Number of Records'=>'Anzahl Daten',
+	'First Recorded Date' => 'Startdatum',
+	'Last Recorded Date' => 'Enddatum',
+	'search' => 'Suche',
+  'first' => 'erste',
+  'prev' => 'vorherige',  
+	'last' => 'letzte',
+  'next' => 'nächste',
+  'My Preferred Locality'=>'Mein bevorzugter Fundort',
+	'My rejected or queried records' => 'Meine zurückgewiesenen Daten',
+	'title'=>'Verbreitungskarte 1',
+      'category' => 'Berichte',      
+      'description'=>'Erstellt eine Verbreitungskarte using Indicia data from GeoServer. Can output a map for a single species '.
+          'or all data from a website. Also features clicking on the data points to see details.',
+	'The distribution map cannot be displayed without a taxon identifier'=>'Die Verbreitungskarte kann nicht ohne eindeutigen Taxname erstellt werden'
+
+);
